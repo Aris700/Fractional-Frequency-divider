73d7eb23acfdc38c9cfe6824679654ff43f08bda
Aris700 <56799147+Aris700@users.noreply.github.com>
2025-02-08 01:38:47 -0500

    Update 16_bit_Accumulator.v
    correction

--- 16_bit_Accumulator.v
+++ 16_bit_Accumulator.v
@@ -1,11 +1,11 @@
 `include "adder_16bit.v"
 `include "pipo_16bit.v"
-module accumulator_16_bit(a,clk,bd,c,rst);
+module accumulator_16_bit(a,clk,b,c,rst);
 input [15:0]a;
 input clk,rst;
-output reg [15:0]bd;
+output [15:0]b;
 output [3:0]c;
-wire [15:0]b;
+reg [15:0]bd;
 adder_16bit A1(a,bd,b,c);
 always @(posedge clk ) begin
     if (rst==1) begin
